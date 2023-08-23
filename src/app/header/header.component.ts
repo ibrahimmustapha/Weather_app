acd149e5071c4c4283c91e02af48c6156a29d308
ibrahim-mustapha <musib581@gmail.com>
2023-08-23 11:32:01 +0000

    excess apis removed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,66 +7,7 @@ import { faDiscord, faGithub, faLinkedin, faTwitterSquare } from '@fortawesome/f
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  githubLink = 'https://img.icons8.com/?size=512&id=AZOZNnY73haj&format=png';
-  twitterLink = 'https://img.icons8.com/?size=512&id=8824&format=png';
-  allTodos: any;
-  books: any;
-  products: any;
-  name = '';
-  message = '';
-  randomQuote: any;
+export class HeaderComponent  {
   githubIcon = faGithub;
   twitterIcon = faTwitterSquare;
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.getTodos();
-    this.getBooks();
-    this.getProducts();
-    this.getRandomQuotes();
-  }
-
-  getTodos() {
-    this.http
-      .get('https://jsonplaceholder.typicode.com/photos')
-      .subscribe((todos) => {
-        this.allTodos = todos;
-      });
-  }
-
-  getBooks() {
-    this.http
-      .get('http://localhost:4200/assets/books.json')
-      .subscribe((books) => {
-        this.books = books;
-      });
-  }
-
-  getProducts() {
-    this.http
-      .get('http://localhost:4200/assets/products.json')
-      .subscribe((response) => {
-        this.products = response;
-      });
-  }
-
-  greetNewCustomer() {
-    if (this.name.trim().length === 0)
-      return (this.message = 'Please enter you name :)');
-
-    return (this.message = `Welcome to CodeMicky Tv Mr/Mrs/Miss ${this.name}`);
-  }
-
-  getRandomQuotes() {
-    this.http
-      .get('http://localhost:4200/assets/quotes.json')
-      .subscribe((response) => {
-        const quotesArray = response as any[];
-        const randomIndex = Math.floor(Math.random() * quotesArray.length);
-        this.randomQuote = quotesArray[randomIndex];
-        console.log(this.randomQuote);
-      });
-  }
 }
